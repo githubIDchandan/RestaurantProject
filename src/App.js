441e630a795583e4873360fa0c11b44abf648d0f
chandanK <iamchandankumar02@gmail.com>
2024-10-10 12:55:49 +0530

    gitSummary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 
 
@@ -9,6 +10,7 @@ const AppLayout=()=>{
     return(
         <div>
             <Header/>
+            <Body/>
         </div>
     )
 }
---
+++ src/components/Body.js
@@ -0,0 +1,15 @@
+
+
+const Body=()=>{
+    return(
+        <div className="res-summary-parent">
+            <div className="res-summary">
+                <h1 className="res-summary-heading">Delicious Food , Delivered To You</h1>
+                <h3 className="res-summary-heading">Choose your favrite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</h3>
+                <h3 className="res-summary-heading">All our meals are cooked with high-quaility ingrediants , just-in-time and of course by experience chefs! </h3>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
