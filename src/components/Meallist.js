5d45d649c3a69ee48b715147afd51dc9cbfba69f
chandanK <iamchandankumar02@gmail.com>
2024-10-10 15:31:30 +0530

    Adding button added

--- src/components/Meallist.js
+++ src/components/Meallist.js
@@ -4,12 +4,21 @@
 const Meallist=(props)=>{
     console.log(props)
     return(
-        <div className="meallistparent">
+       <div className="mealsP">
+         <div className="meallistparent">
           <h2 className="mlist">{props.mealsData.name}</h2>
           <p className="mlist">{props.mealsData.cuisine}</p>
           <h2 className="mlist">{props.mealsData.price}</h2>
           
         </div>
+        <div>
+            <div className="amountcounter">
+            <h2 className="amounttag">Amount</h2>
+            <button className="counterBtn">1</button>
+            </div>
+            <button className="addBtn">+Add</button>
+        </div>
+       </div>
     )
 }
 
